76f54d7af0ec31f59f8f2b66cbba373ca7bf4013
llucas.spot <llucas.spot@gmail.com>
2025-01-02 02:19:13 +0100

    init: vite react ts project

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// TODO
+const basepath = process.env.VITE_BASE_PATH ?? '/';
+console.log('basepath : ', basepath);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: basepath,
+  server: {
+    host: '0.0.0.0',
+    port: 3101,
+  },
+  resolve: {
+    alias: {},
+  },
+})
